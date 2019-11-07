c0a5f7b51fb57af8a29e8220101553d67a9419ee
Viet Tran <tranhuyviet@gmail.com>
2019-11-07 20:06:08 +0200

    structures signin, signup and createproject

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import DashBoard from "./components/dashboard/Dashboard";
 import ProjectDetails from "./components/projects/ProjectDetails";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
+import CreateProject from "./components/projects/CreateProject";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Switch>
           <Route exact path="/" component={DashBoard} />
           <Route path="/project/:id" component={ProjectDetails} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/create" component={CreateProject} />
         </Switch>
       </div>
     </BrowserRouter>
