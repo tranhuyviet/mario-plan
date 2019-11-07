e2c510e90b68dcd2ab7235ea6d50af13af50784a
Viet Tran <tranhuyviet@gmail.com>
2019-11-07 15:16:41 +0200

    structures dashboard, projectlist and notifications components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import DashBoard from "./components/dashboard/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
+        <Switch>
+          <Route path="/" component={DashBoard} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
