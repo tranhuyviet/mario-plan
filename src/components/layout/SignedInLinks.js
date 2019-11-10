343783e8b31e4b84859de9433e324500a3349270
Viet Tran <tranhuyviet@gmail.com>
2019-11-10 16:33:37 +0200

    added firebase functions, implement notifications and finish all functionalities of app

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -15,7 +15,7 @@ const SignedInLinks = props => {
       </li>
       <li>
         <NavLink to="/" className="btn btn-floating pink lighten-1">
-          VT
+          {props.profile.initials}
         </NavLink>
       </li>
     </ul>
