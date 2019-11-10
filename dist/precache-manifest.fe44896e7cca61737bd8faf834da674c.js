419c22023bd887fd8c2823440af9195e076a19b5
Viet Tran <tranhuyviet@gmail.com>
2019-11-10 16:49:28 +0200

    build and deploy the app

---
+++ dist/precache-manifest.fe44896e7cca61737bd8faf834da674c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4629b92a80f99eb1879a38184841e6b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e82cc95c5adaebff58c8",
+    "url": "/static/css/main.5dbe4f54.chunk.css"
+  },
+  {
+    "revision": "2a5833c413bcd16de3e0",
+    "url": "/static/js/2.ccd25f11.chunk.js"
+  },
+  {
+    "revision": "e82cc95c5adaebff58c8",
+    "url": "/static/js/main.c2bf3a13.chunk.js"
+  },
+  {
+    "revision": "eb6a028cc3a9d3fd043c",
+    "url": "/static/js/runtime-main.69c24cc5.js"
+  }
+]);
